d005bcbeb33f40270dea02caf0303ef0074b3501
Jonathan Chasteen <jonathan.chasteen@live.com>
2023-01-06 18:29:30 -0500

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Jonathan Chasteen on 1/6/23.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
